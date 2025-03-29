a43ec0f70dfda5b67474507d4f1b6e159af28a74
leofabrr <leofabrr@gmail.com>
2025-03-29 14:29:46 -0300

    app bar

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -9,8 +9,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Lista de Tarefas')),
+        body: const Center(child: Text('Hello World!')),
+      ),
     );
   }
 }
