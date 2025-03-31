3cf28871970cada9b9e4c9756918b4e322b0698b
leofabrr <leofabrr@gmail.com>
2025-03-31 16:16:15 -0300

    feat: primeira implementação do Usuário Model
    Campos id, nome e email com validação criados
    
    Signed-off-by: leofabrr <leofabrr@gmail.com>

---
+++ biblioteca_app/lib/domain/models/usuario.dart
@@ -0,0 +1,21 @@
+class Usuario {
+  final String id;
+  final String nome;
+  final String email;
+
+  Usuario({required this.id, required this.nome, required this.email}) {
+    _validar();
+  }
+
+  void _validar() {
+    if (nome.isEmpty) {
+      throw ArgumentError('Nome não pode estar vazio');
+    }
+
+    if (!email.contains('@')) {
+      throw ArgumentError('Formado de email inválido');
+    }
+  }
+
+  String get nomeFormatado => nome.toUpperCase();
+}
